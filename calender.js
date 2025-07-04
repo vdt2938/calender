71656cb611289212cfadbed210e09c4dded420f9
Van Taylor <vdt.jr2938@gmail.com>
2025-07-04 16:39:01 -0500

    it get better

--- calender.js
+++ calender.js
@@ -1,4 +1,4 @@
-
+// clock
 let clock = () => {
     let date = new Date();
     let hrs = date.getHours();
@@ -45,13 +45,11 @@ let clock = () => {
   };
 clock();
 
-let d = () =>{
-//get all of date from below to get into d function
-}
-
+// calender
 let date = new Date()
 let year = date.getFullYear()
-
+let month_class = document.getElementsByClassName('month');
+const week = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
 let currentlymonth = date.getMonth()  
 let month_name = [
   'January','February','March',
@@ -60,8 +58,10 @@ let month_name = [
   'October','November','December'
 ]
 
-let month_class = document.getElementsByClassName('month');
-const week = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+// it tell what is current month
+month_class[currentlymonth].style.background = 'gray'
+month_class[currentlymonth].style.color = 'white'
+
 
 for(i = 0;i<month_name.length;i++){
   /*
@@ -70,21 +70,55 @@ for(i = 0;i<month_name.length;i++){
   create h3 
  */
   try_element = ['h1','h2','h3','h4','h5','h6','p','span']
-  h3 = document.createElement(try_element[6])
+  h3 = document.createElement(try_element[1])
   text = document.createTextNode(month_name[i])
   h3.appendChild(text)
-  const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
-
-  console.log(month_name[i]+' have '+daysInMonth(year, i)+' days '); // 31
-  po = month_name[i]+' have '+daysInMonth(year, i)+' days '
   month_class[i].appendChild(h3)
-  
+  // month_class in html. Jan to Dec
+
+  // it tell how many day for each month have
+
+  const daysInMonth = (year, month) => new Date(year, month, 0).getDate();//function
+
+  console.log(month_name[i]+' have '+daysInMonth(year, i+1)+' days '); // jan have 31 days
+  x = month_name[i]+' have '+daysInMonth(year, i+1)+' days '
+  // i need to firgure it out how to apply it 
+  month_class[i].innerHTML += x
+  //sample
+  jan = month_name[0]+' have '+daysInMonth(year, 1)+' days '
+  feb = month_name[1]+' have '+daysInMonth(year, 2)+' days '
+  mar = month_name[2]+' have '+daysInMonth(year, 3)+' days '
+  apr = month_name[3]+' have '+daysInMonth(year, 4)+' days '
+  may = month_name[4]+' have '+daysInMonth(year, 5)+' days '
+  jun = month_name[5]+' have '+daysInMonth(year, 6)+' days '
+  jul = month_name[6]+' have '+daysInMonth(year, 7)+' days '
+  aug = month_name[7]+' have '+daysInMonth(year, 8)+' days '
+  sep = month_name[8]+' have '+daysInMonth(year, 9)+' days '
+  oct = month_name[9]+' have '+daysInMonth(year, 10)+' days '
+  nov = month_name[10]+' have '+daysInMonth(year, 11)+' days '
+  dec = month_name[11]+' have '+daysInMonth(year, 12)+' days '
+ 
   
 }
+/* 
+month_class[0].innerHTML += jan
+month_class[1].innerHTML += feb
+month_class[2].innerHTML += mar
+month_class[3].innerHTML += apr
+month_class[4].innerHTML += may
+month_class[5].innerHTML += jun
+month_class[6].innerHTML += jul
+month_class[7].innerHTML += aug
+month_class[8].innerHTML += sep
+month_class[9].innerHTML += oct
+month_class[10].innerHTML += nov
+month_class[11].innerHTML += dec 
+*/
 
-month_class[currentlymonth].style.background = 'green'
+
 let page = document.getElementsByClassName("page")[0]
 let weekday = document.getElementsByClassName("week")[0]
+
 for(i=0;i<31;i++){
   jo = `<div class="day">${i+1}</div>`
   console.log(jo)
@@ -96,4 +130,3 @@ for(i=0;i<week.length;i++){
   console.log(jo)
   weekday.innerHTML += jo
 }
-//daysInMonth(2024, 2); // 29
